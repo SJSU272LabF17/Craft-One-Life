da3d6cfbc86a6b03da58c1ac44416f7f3c409344
Kalikalyan Dash <kkdgrece@gmail.com>
2017-11-29 00:34:18 -0800

    Added about us content

---
+++ CraftOneLife/react/src/components/AboutUs.js
@@ -0,0 +1,89 @@
+import React, {Component} from 'react';
+import aboutus from '../images/aboutus.jpg';
+
+class AboutUs extends Component{
+    render(){
+        return(
+            <div className="row justify-content-md-center">
+
+            <div className="container">
+            <div>
+            <h1 style="text-align: center; margin: 28px;">About Us</h1>
+        </div>
+
+        <div className="row">
+            <div className="col-lg-2"></div>
+            <div className="col-lg-8">
+            <div className="row">
+            <div className="col-lg-4">
+            <img src="https://cdn.shopify.com/s/files/1/0950/0728/files/cycle-of-empowerment.jpg?18063852365609462859" style="width: 200px; height: 200px"alt="cycle of art lifting empowerment"></img>>
+            </div>
+            <div className="col-lg-8" style="color: #666666; margin-bottom: 40px; line-height:1.5em; font-size: 1.375em; vertical-align: beseline; float: right;text-align: left">
+            <p>ArtLifting empowers artists living with homelessness or disabilities through the celebration and sale of their artwork. ArtLifting is about creating opportunity, empowerment, and validation.</p>
+        <p>We offer our artists the chance to secure their own income through the sale of original paintings, prints, and products. By showcasing and selling artwork via ArtLifting.com, our artists gain self-confidence that permeates all aspects of their lives.</p>
+        </div>
+        </div>
+
+        </div>
+        <div className="col-lg-2"></div>
+            </div>
+
+            <div className="row">
+            <div className="col-lg-2"></div>
+            <div className="col-lg-8">
+            <div className="row">
+            <div className="col-lg-4">
+            <img src="https://cdn.shopify.com/s/files/1/0950/0728/files/Screen_Shot_2017-03-24_at_11.04.17_AM.png?v=1490368021" style="width: 250px; height: 200px"alt="cycle of art lifting empowerment"></img>>
+            </div>
+            <div className="col-lg-8" style="color: #666666; margin-bottom: 40px; line-height:1.5em; font-size: 1.375em; vertical-align: beseline; float: right;text-align: left">
+            <p>As a social enterprise and benefit corporation, we are using “business for good.” Every artist earns 55% from the profit of each sale. 1% from each sale goes toward strengthening art services for community partners that support ArtLifting artists, which includes art programming at social service agencies, shelters, and disability centers. ArtLifting uses the remaining 44% to further our mission. We realize that in order to make sustainable change, we need to run a financially sustainable business.</p>
+        <img alt="Quick Facts" src="//cdn.shopify.com/s/files/1/0950/0728/files/QuickFacts_updated_1337410e-8782-4d20-a3c8-b785e6002c3b_2048x2048.jpg?v=1493407761" style="float: none; display: block; margin-left: auto; margin-right: auto; width: 500px"></img>>
+            </div>
+            </div>
+
+            </div>
+            <div className="col-lg-2"></div>
+            </div>
+
+            <div>
+            <h1 style="text-align: center; margin: 28px;">OUR STORY</h1>
+        </div>
+        <div className="row">
+            <div className="col-lg-2"></div>
+            <div className="col-lg-8">
+            <div className="row">
+            <div className="col-lg-4">
+            <img src="https://cdn.shopify.com/s/files/1/0950/0728/files/liz1_grande.jpg?18063852365609462859" style="width: 250px; height: 250px"alt="cycle of art lifting empowerment"></img>>
+            </div>
+            <div className="col-lg-1"></div>
+            <div className="col-lg-7" style={{color: "#666666"; margin-bottom: "40px"; line-height:1.5em; font-size: 1.375em; vertical-align: beseline; float: right;text-align: left">
+        <p>Liz Powers began her mission in Boston when she was 18.</p>
+        <p>After securing a grant from Harvard to create art groups in homeless shelters, Liz was immediately struck by the incredible talent that she saw. Amazing works of art were being produced only to end up forgotten in the closets and basements of shelters. After observing that the same problem existed at thousands of art programs across the country, Liz became committed to helping share their creations with the world. She realized that art could be a transformational path to financial stability for these artists and countless others nationwide; they just needed a digital marketplace to connect with a larger audience. This invisible talent needed to become visible.</p>
+        </div>
+        </div>
+
+        </div>
+        <div className="col-lg-2"></div>
+            </div>
+
+            <div className="row">
+            <div className="col-lg-2"></div>
+            <div className="col-lg-8">
+            <div className="col-lg-4">
+            <img src="https://cdn.shopify.com/s/files/1/0950/0728/files/Liz-and-Spencer-Powers.jpg?18063852365609462859" style="width: 250px; height: 250px"alt="cycle of art lifting empowerment"></img>
+            </div>
+            <div className="col-lg-1"></div>
+            <div className="col-lg-7" style="color: #666666; margin-bottom: 40px; line-height:1.5em; font-size: 1.375em; vertical-align: beseline; float: right;text-align: left">
+            <p>In November of 2013, Liz and her brother Spencer Powers founded ArtLifting with four Boston artists. During their first week of operations, those four artists earned thousands of dollars and were interviewed by national and local media—truly a life-changing experience for each of them. Today, ArtLifting has expanded to 19  cities across the United States.</p>
+        </div>
+        </div>
+        <div className="col-lg-2"></div>
+            </div>
+            </div>
+
+
+            </div>
+        )
+    }
+}
+export default AboutUs;
