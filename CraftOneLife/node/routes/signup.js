4b97795aec812894b7574bcd3357f3af72a332ce
Divyank <divyank68@yahoo.co.in>
2017-12-01 15:22:32 -0800

    adding checksignup function

--- CraftOneLife/node/routes/signup.js
+++ CraftOneLife/node/routes/signup.js
@@ -4,4 +4,9 @@ var express = require('express');
 var doSignUp = function(req,res) {
     console.log("in DO signup of node" + req.body);
 };
+
+
+var checksignup = function(req,res) {
+    console.log("in DO checksignup of node" + req.body);
+};
 exports.doSignUp=doSignUp;
