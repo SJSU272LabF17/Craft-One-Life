879aeaacc77a6f1bb4781c9614a708f8a551522c
nsjethani <neha.jethani6627@gmail.com>
2017-12-08 16:30:49 -0800

    Admin code for approve user/books

--- CraftOneLife/react/src/components/AdminApproveBooks.js
+++ CraftOneLife/react/src/components/AdminApproveBooks.js
@@ -4,67 +4,105 @@ import '../css/bootstrap.css';
 import * as API from '../api/API'
 
 
-class AdminApproveBooks extends Component{
+class AdminApproveBooks extends Component {
 
 
-    state={
-        Books:[]
+    state = {
+        Books: []
     };
 
-    componentWillMount(){
+    componentWillMount() {
         API.getBooksForApproval().then((res) => {
-            alert(JSON.stringify(res.resarray));
-            this.setState({Books:res.resarray});
+            alert(res.resarray);
+            if (typeof res.resarray == "undefined") {
+                res.resarray = [];
+                this.setState({Books: res.resarray});
+            }
+            else {
+                this.setState({Books: res.resarray});
+            }
         });
     }
 
-    addApprovedBooks(id)
-    {
+    addApprovedBooks(u) {
 
-        alert(id);
-        var data={id:id};
+        alert(u);
+        var data = {u: u};
         API.approveBooks(data).then((res) => {
             alert("Book approved successfully");
+            API.getBooksForApproval().then((res) => {
+                alert(res.resarray);
+                if (typeof res.resarray == "undefined") {
+                    res.resarray = [];
+                    this.setState({Books: res.resarray});
+                }
+                else {
+                    this.setState({Books: res.resarray});
+                }
+            });
+        });
+    }
+
+    rejectBooks(u) {
+
+        alert(u);
+        var data = {u: u};
+        API.rejectBooks(data).then((res) => {
+            alert("Book rejected successfully");
+            API.getBooksForApproval().then((res) => {
+                alert(res.resarray);
+                if (typeof res.resarray == "undefined") {
+                    res.resarray = [];
+                    this.setState({Books: res.resarray});
+                }
+                else {
+                    this.setState({Books: res.resarray});
+                }
+            });
         });
     }
 
     render() {
         return (
-            <div className="col-sm-4 col-md-4">
-                <h1>Hi</h1>
+            <div className="col-sm-8 col-md-8">
+                <br/>
 
-                <h4>Books to be approved</h4>
-                <h4>Approved Books</h4>
-                {this.state.Books.map((u,index) =>(
+                <h3 style={{color: 'green'}}><u>Books to be approved</u></h3>
+                <hr/>
+                <br/>
+                {this.state.Books.map((u, index) => (
                     <div>
 
 
-                    <div className="list-group-item clearfix" >
-                        <div className="pull-left">
-                            NAME : <a className="list-group-item-heading" href={'http://localhost:3001/uploads/'+u.book_name}>{u.book_name}</a>
+                        <div className="list-group-item clearfix">
+                            <div className="pull-left">
+                                <h3> NAME :</h3> <a className="list-group-item-heading"
+                                                    href={'http://localhost:3001/uploads/' + u.book_path.split("/")[3]}>{u.book_name}</a>
 
-                        </div>
+                            </div>
 
-                        <div className="pull-right">
-                            <div className="row">
-                                <div className="col-xs-12">
-                                    <button className="btn btn-success"  onClick={()=>this.addApprovedBooks(u.book_id)}> APPROVE</button>
-                                    <button className="btn btn-danger"> REJECT</button>
+                            <div className="pull-right">
+                                <div className="row">
+                                    <div className="col-xs-12">
+                                        <button className="btn btn-success" onClick={() => this.addApprovedBooks(u)}>
+                                            APPROVE
+                                        </button>
+                                        &nbsp;
+                                        <button className="btn btn-danger" onClick={() => this.rejectBooks(u)}> REJECT
+                                        </button>
 
 
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    </div>
 
-                    /*<tr className="row" key={u.book_id} >
-                        <td>{u.book_name}</td>
-                        <td><button onClick={()=>this.addApprovedBooks(u.book_id)} className="btn btn-primary btn-sm"> Approve</button></td>
-                    </tr>*/
-                ) )}
+
+                ))}
             </div>
         );
     }
 }
+
 export default AdminApproveBooks;
--- CraftOneLife/react/src/components/AdminApproveUsers.js
+++ CraftOneLife/react/src/components/AdminApproveUsers.js
@@ -23,8 +23,10 @@ class AdminApproveUsers extends Component{
             alert(res.resarray);
             if(typeof res.resarray=="undefined"){
                 res.resarray=[];
+                this.setState({users:res.resarray});
             }
-            this.setState({users:res.resarray});
+            else{
+            this.setState({users:res.resarray});}
         });
     }
 
@@ -33,6 +35,23 @@ class AdminApproveUsers extends Component{
         var data={email:u.email};
 
         API.approveuser(data).then((res) => {
+
+            //alert("User approved successfully");
+            API.getusersforapproval().then((res) => {
+                alert(res.resarray);
+                if(typeof res.resarray=="undefined"){
+                    res.resarray=[];
+                }
+                this.setState({users:res.resarray});
+            });
+        });
+    }
+
+    rejectUser(u)
+    {
+        var data={email:u.email};
+
+        API.rejectuser(data).then((res) => {
 
             //alert("User approved successfully");
             API.getusersforapproval().then((res) => {
@@ -55,11 +74,13 @@ class AdminApproveUsers extends Component{
 
     render() {
         return (
-            <div className="col-sm-4 col-md-4">
-                <h1>Hi</h1>
+            <div className="col-sm-8 col-md-8">
+<br/>
 
-                <h4>Users to be approved</h4>
-                <h4>Approved Users</h4>
+                <h3 style={{color:'green'}}><u>Users to be approved</u></h3>
+                <hr/>
+                <br/>
+
                 {this.state.users.map((u,index) =>(
                     <div>
 
@@ -74,7 +95,7 @@ class AdminApproveUsers extends Component{
                                 <div className="row">
                                     <div className="col-xs-12">
                                         <button className="btn btn-success"  onClick={()=>this.addApprovedUser(u)}> APPROVE</button>
-                                        <button className="btn btn-danger"> REJECT</button>
+                                        &nbsp;<button className="btn btn-danger" onClick={()=>this.rejectUser(u)}> REJECT</button>
 
 
                                     </div>
