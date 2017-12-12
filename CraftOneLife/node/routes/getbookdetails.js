5f12cff904ceee89f8d90d3668705b8f05429086
nsjethani <neha.jethani6627@gmail.com>
2017-12-11 23:50:28 -0800

    Adding artist story on book click

--- CraftOneLife/node/routes/getbookdetails.js
+++ CraftOneLife/node/routes/getbookdetails.js
@@ -9,7 +9,7 @@ var getBookDetails =  function (req,res) {
     //var get_profile = "select * from books where book_name="+"'"+bookname+"'";
     //
 
-    var get_books="select * from books where isApproved='1'";
+    var get_books="select * from books b,users u where isApproved='1' and u.user_id=b.user_id";
 
     try {
         mysql.fetchData(function (err, results) {
@@ -19,17 +19,27 @@ var getBookDetails =  function (req,res) {
             else {
                 //console.log("Book Details ",results[0]);
                 //var bitmap = fs.readFileSync(results[0].bookTilePath);
+                var authorImage="";
                 var resultFiles = [];
                 for(var i=0;i<results.length;i++)
                 {
                     var bitmap = fs.readFileSync(results[i].bookTilePath);
+
+                    if(results[i].profile_pic===null || results[i].profile_pic==="" || results[i].profile_pic==="undefined")
+                    {
+                        authorImage=""
+                    }else
+                    {
+                        authorImage=(new Buffer(fs.readFileSync(results[i].profile_pic)).toString('base64'))
+                    }
 
                     var data={
                     bookId:results[i].book_id,
                     bookName:results[i].book_name,
                     bookPrice:results[i].admin_price,
                     bookDesc:results[i].book_desc,
-                    bookTilePath:(new Buffer(bitmap).toString('base64'))
+                    bookTilePath:(new Buffer(bitmap).toString('base64')),
+                    aboutAuthor:results[i].about_me,
                 }
                 resultFiles.push(data);
                 }
--- CraftOneLife/react/src/components/BooksAdventure.js
+++ CraftOneLife/react/src/components/BooksAdventure.js
@@ -3,11 +3,14 @@ import * as API from '../api/API';
 import ImageGridList from "./ImageGridList";
 import TextField from 'material-ui/TextField';
 import Typography from 'material-ui/Typography';
+import {Modal} from 'react-bootstrap';
+import Avatar from 'react-avatar';
 
 class BooksAdventure extends Component{
 
     state={
-        books:[]
+        books:[],
+        show_modal:false,
     };
 
 
@@ -22,6 +25,14 @@ class BooksAdventure extends Component{
             });
 
 
+    };
+
+    open = () => {
+        this.setState({show_modal: true})
+    }
+
+    close = () => {
+        this.setState({show_modal: false});
     };
 
     addToCart = (bookdata) => {
@@ -52,6 +63,65 @@ class BooksAdventure extends Component{
             <div className="row justify-content-md-center">
                 <div className="col-md-12">
 
+                    <Modal show={this.state.show_modal} onHide={() => {
+                        this.close()
+                    }}>
+                        {/* <Modal.Header closeButton>
+                        <Modal.Title>Login</Modal.Title>
+                    </Modal.Header>*/}
+                        <Modal.Body>
+                            <div className="col-sm-12 col-md-12">
+                                <div>
+                                    <Avatar src={"data:image/jpeg;base64,"+this.state.aboutAuthor} round={true} size={150} alt={this.state.profile_pic}/>
+                                </div>
+
+                                <div className="col-sm-4 col-md-4"><p>About Author : </p></div>
+                                <div className="col-sm-8 col-md-8">
+
+
+
+                                    <div className="input-field">
+                                        <input className="form-control"
+                                               style={{height:150, textAlign:'top'}}
+
+                                               type="text"
+                                               disabled={true}
+                                               value={this.state.aboutAuthor}
+                                               onChange={(event)=>{
+                                                   this.setState({
+                                                       about_me:event.target.value
+                                                   });
+                                               }}
+                                               required
+
+                                        />
+                                    </div>
+                                    <br/>
+                                </div>
+
+                                <div className="col-sm-4 col-md-4"><p>About Book : </p></div>
+                                <div className="col-sm-8 col-md-8">
+
+
+
+                                    
+                                </div>
+                                <br/>
+
+                            </div>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <div className="col-sm-10 col-md-10">
+
+
+                                <button onClick={() => {
+                                    this.close()
+                                }}>Close
+                                </button>
+                            </div>
+                        </Modal.Footer>
+                    </Modal>
+
                     { this.state.books.map(tile => (
 
 
@@ -59,7 +129,7 @@ class BooksAdventure extends Component{
                             <br/>
                             <div className="list-group-item clearfix" style={{height:215,width:250}}>
                                 <div className="pull-right">
-                                    <img src={"data:image/jpeg;base64,"+tile.bookTilePath} height={100} width={200} alt={tile.bookTilePath} />
+                                    <img src={"data:image/jpeg;base64,"+tile.bookTilePath} height={100} width={200} alt={tile.bookTilePath} onClick={()=>{this.open()}} />
 
                                 </div>
                                 <div className="pull-right" style={{textAlign:'center', width:200}}>
